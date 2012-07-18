468c775d2fa5c53130b8993bd8a06f7f0c3c2dd5
Christopher Thielen <cthielen@gmail.com>
2012-07-18 10:54:21 -0700

    Added application model

--- models/application.rb
+++ models/application.rb
@@ -1,6 +1,6 @@
 require 'active_resource'
 
-class Person < ActiveResource::Base
+class Application < ActiveResource::Base
   begin
     rm_settings = YAML.load_file("config/dss-rm.yml")['dssrm']
 
@@ -9,15 +9,5 @@ class Person < ActiveResource::Base
     self.password = rm_settings["password"]
   rescue Errno::ENOENT => e
     puts "config/dss-rm.yml is missing. Please fix (see config/dss-rm.example.yml)."
-  end
-
-  # Required by declarative_authorization to determine access
-  def role_symbols
-    roles.map { |x| x.to_sym }
-  end
-
-  # Returns true if the given symbol is in this.role_symbols
-  def has_role?(role_sym)
-    role_symbols.include? role_sym
   end
 end

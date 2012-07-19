b8aeb5a6bbebc90ec539829ba5b472e293c5f9bd
Christopher Thielen <cthielen@gmail.com>
2012-07-19 11:33:38 -0700

    Basic logic layout

--- sympa-sync.rb
+++ sympa-sync.rb
@@ -1,5 +1,14 @@
+load 'models/application.rb'
 load 'models/person.rb'
-require 'pp'
 
-p = Person.find("cthielen")
-pp p
+# Fake Sympa lists (to be replaced with Sympa API calls later)
+sympa_lists = ['history-staff', 'history-fac', 'dssit-devs', 'econ-all']
+cached_sympa_lists = {} # used to add additional properties, e.g. 'touched'
+
+# Get a list of all applications
+applications = Application.all
+
+# Go through each application and sync with the lists
+
+
+# Check to see if any lists weren't synced (new lists)

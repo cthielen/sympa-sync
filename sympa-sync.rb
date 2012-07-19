cc37862c6ef02fd36d0c8bd28755182c25319acf
Christopher Thielen <cthielen@gmail.com>
2012-07-19 14:48:42 -0700

    Just committing for a sync

--- sympa-sync.rb
+++ sympa-sync.rb
@@ -1,5 +1,6 @@
 load 'models/application.rb'
 load 'models/person.rb'
+require 'pp'
 
 # Fake Sympa lists (to be replaced with Sympa API calls later)
 sympa_lists = ['history-staff', 'history-fac', 'dssit-devs', 'econ-all']
@@ -8,7 +9,18 @@ cached_sympa_lists = {} # used to add additional properties, e.g. 'touched'
 # Get a list of all applications
 applications = Application.all
 
-# Go through each application and sync with the lists
+pp applications
 
+abort
+
+# Go through each application and sync with the lists
+applications.each do |application|
+  puts application.name
+  if sympa_lists.include? application.name
+    puts "found in list"
+  else
+    puts "not found in list"
+  end
+end
 
 # Check to see if any lists weren't synced (new lists)
